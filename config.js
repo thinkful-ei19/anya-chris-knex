03bf7ae3fbb3ad89588b82883d3ce312e9c41eba
Anya <anya.p.nguyen@gmail.com>
2018-03-13 09:24:05 -0700

    added knex queries

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const DATABASE_URL = process.env.DATABASE_URL
 exports.DATABASE = {
   client: 'pg',
   connection: DATABASE_URL,
-  pool: { min: 0, max: 3 }, // Fix issue w/ ElephantSQL
-  debug: true               // Outputs knex debugging information
+  pool: { min: 1, max: 2 }, // Fix issue w/ ElephantSQL
+  debug: false               // Outputs knex debugging information
 };
 
 exports.PORT = process.env.PORT || 8080; 
--- drills.js
+++ drills.js
@@ -7,15 +7,30 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
+
+
+
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .then(results => console.log(results));
+
 knex
   .select()
   .from('restaurants')
+  .where('restaurants.cuisine', 'Italian')
   .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
-
+  .then(([results]) => console.log(results));
 
 // Destroy the connection pool
-knex.destroy().then(() => {
-  console.log('database connection closed');
-});
+// knex.destroy().then(() => {
+//   console.log('database connection closed');
+// });
